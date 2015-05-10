e0d42424f0e55ea9a4e8698e1d49b2c809aebe34
origalin <2360436350@qq.com>
2015-05-10 20:45:36 +0800

    恢复

---
+++ Game1/src/ui/Game.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class Game {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GameWindow tbc = new GameWindow();
+
+		tbc.setVisible(true);
+
+	}
+
+}
