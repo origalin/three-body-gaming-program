494602863e4356bbc3eaf6cfd1858fb17cec13d3
874855266@qq.com <874855266@qq.com>
2015-05-15 11:19:13 +0800

    未完成的物品面板

---
+++ Game1/src/ui/ObjectPanel.java
@@ -0,0 +1,44 @@
+package ui;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
+public class ObjectPanel extends JLabel{
+	ImageIcon start1 = new ImageIcon("image/button1.png");
+	ImageIcon start2 = new ImageIcon("image/button2.png");
+	ImageIcon start3 = new ImageIcon("image/button3.png");
+	ImageIcon icon1 = new ImageIcon("image/iconblue1.png");
+	ImageIcon icon2 = new ImageIcon("image/iconblue2.png");
+	ImageIcon icon3 = new ImageIcon("image/iconblue3.png");
+	ImageButton confirmButton = new ImageButton(start1, start2, start3, false);
+	public ObjectPanel(ImageIcon i) {
+		super(i);
+		setBounds(240, 28, i.getIconWidth(), i.getIconHeight());
+		setOpaque(false);
+		
+		
+		confirmButton.setLocation(70, 530);
+		confirmButton.setText("确认");
+		confirmButton.setHorizontalTextPosition(JButton.CENTER);
+		confirmButton.setVerticalTextPosition(JButton.CENTER);
+		confirmButton.setFont(new Font("微软雅黑", Font.BOLD, 20));
+		confirmButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 自动生成的方法存根
+				setVisible(false);
+			}
+		});
+		add(confirmButton);
+		JScrollPane tecScrollPane =new JScrollPane();
+		tecScrollPane.setBounds(50, 20, 500, 500);
+		add(tecScrollPane);
+	}
+}
