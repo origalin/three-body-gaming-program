6d0d46c2b4ebb08fb507b1a8840c59805a4a5f2e
lizhimu <2748738706@qq.com>
2015-05-16 22:41:01 +0800

    fuck

--- Game1/src/el/Theory.java
+++ Game1/src/el/Theory.java
@@ -2,14 +2,14 @@ package el;
 
 public class Theory {
 	
-	int mathPoint = 0;
-	int physicsPoint = 0;
-	int biologyPoint = 0;
-	int chemistryPoint = 0;
-	int artPoint  = 0;
-	int computerPoint = 0;
-	boolean isClicked = false;//如果被点就能点物品
-	boolean isAvailable = false;//如果点数够就会发光
+	public int mathPoint = 0;
+	public int physicsPoint = 0;
+	public int biologyPoint = 0;
+	public int chemistryPoint = 0;
+	public int artPoint  = 0;
+	public int computerPoint = 0;
+	public static boolean isClicked = false;//如果被点就能点物品
+	public static boolean isAvailable = false;//如果点数够就会发光
 	
 	public Theory(){
 		
@@ -22,13 +22,8 @@ public class Theory {
 		chemistryPoint = chemistry;
 		artPoint = art;
 		computerPoint = computer;
-		Sci.math.point -= 14;
-		Sci.physics.point -= 11;
-		Sci.biology.point -= 11;
-		Sci.chemistry.point -= 11;
-		Sci.art.point -= 12;
-		Sci.computer.point -= 10;
 	}
+	
 	
 	public void judgeC(Theory theory){
 //		if(theory.isClicked==true)
@@ -46,10 +41,8 @@ public class Theory {
 		return false;
 	}
 	
-	public void judgeSumA(){//点了从主界面进入科技树的按钮时调用
-		fly.isAvailable = fly.judgeA(fly);//把所有理论放在一个arraylist中，遍历arraylist
-	}
 	
-	Theory fly = new Theory(1,2,3,4,5,6);
+	
+	
 }
 
