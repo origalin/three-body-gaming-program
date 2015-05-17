c92320a557229f0731a3baca885d093531fb782a
bismuth1102 <951102ljc>
2015-05-18 00:28:04 +0800

    啊哈

--- Game1/src/el/Next.java
+++ Game1/src/el/Next.java
@@ -6,12 +6,14 @@ public class Next {
 	Environment env = new Environment();
 	Happiness hap = new Happiness();
 	Point poi = new Point();
+	Bomb bomb = new Bomb();
 	
 	public void goNext(){
 		eco.economy();
 		env.environment();
 		hap.happiness();
 		poi.point();
+		bomb.bomb();
 	}
 	
 }
