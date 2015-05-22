2e1313dacab17e86a9e29d3fdac9e0cfc49ce226
bismuth1102 <951102ljc>
2015-05-22 11:15:11 +0800

    bomb更改完成

--- Game1/src/el/Bomb.java
+++ Game1/src/el/Bomb.java
@@ -13,11 +13,25 @@ public class Bomb {
 	static String[]economy={"你的星球正在发生饥荒","首都发生地震，急缺赈灾基金","你的居民要求医疗保障，各地游行频发","地方政府已无力偿还债务"};
 	static String[]happiness={"各地正在发生暴乱","医疗中心显示抑郁症患者急剧增多","青少年自杀事故频发","各地犯罪行为不断，监狱人满为患"};
 	static String[]enviroment={"森林的覆盖面积正在迅速下降","草场正在急剧荒漠化","水资源陷入短缺","多地出现极端气候"};
+	static String[]economy2={"","","",""};
+	static String[]happiness2={"","","",""};
+	static String[]enviroment2={"","","",""};
 	
 	public String bomb(){
 		String s = "";
-		if(Begin.EV<40+(GameWindow.time-1020)*1.1&&(int)(Math.random()*3)==2){
-			//Economy will suffer
+		double ev = 15000/(5000-Begin.HV);
+		double hv = 300/(100-Begin.HV);
+		double emv = 300/(100-Begin.EMV);
+		if(Begin.EV>=40+(GameWindow.time-1020)*1.1&&(int)Math.random()*ev==0){
+			Economy.EIR -= (0.001+(int)(Math.random()*4)*0.0002)/ev;
+			Begin.EV -= (20+(int)(Math.random()*4)*3)/ev;
+			Begin.HP -= 5*(int)(Math.random()*4)/ev;
+			if(Begin.HP<=0)
+				Begin.HP = 0;
+			s += economy2[(int)(Math.random()*4)] + "\n";
+		}
+		else if(Begin.EV<40+(GameWindow.time-1020)*1.1&&(int)(Math.random()*3)==2){
+			
 			Economy.EIR -= 0.001+(int)(Math.random()*4)*0.0002;
 			Begin.EV -= 20+(int)(Math.random()*4)*3;
 			Begin.HP -= 5*(int)(Math.random()*4);
@@ -25,20 +39,35 @@ public class Bomb {
 				Begin.HP = 0;
 			s += economy[(int)(Math.random()*4)] + "\n";
 		}
-		if(Begin.HV<60&&(int)(Math.random()*3)==2){
+		
+		if(Begin.HV>=60&&(int)Math.random()*hv==0){
+			Begin.HV -= (10+(int)(Math.random()*4)*2)/hv;
+			Begin.HP -= 5*(int)(Math.random()*4)/hv;
+			if(Begin.HP<=0)
+				Begin.HP = 0;
+			s += happiness2[(int)(Math.random()*4)] + "\n";
+		}
+		else if(Begin.HV<60&&(int)(Math.random()*3)==2){
 			Begin.HV -= 10+(int)(Math.random()*4)*2;
 			Begin.HP -= 5*(int)(Math.random()*4);
 			if(Begin.HP<=0)
 				Begin.HP = 0;
 			s += happiness[(int)(Math.random()*4)] + "\n";
 		}
-		if(Begin.EMV<60&&(int)(Math.random()*3)==2){
-			//Environment will suffer
-			Environment.EMI -= 10+(int)(Math.random()*4)*2;
+		
+		if(Begin.EMV>=60&&(int)Math.random()*emv==0){
+			Begin.EMV -= (10+(int)(Math.random()*4)*2)/emv;
+			Begin.HP -= 5*(int)(Math.random()*4)/emv;
+			if(Begin.HP<=0)
+				Begin.HP = 0;
+			s += economy2[(int)(Math.random()*4)] + "\n";
+		}
+		else if(Begin.EMV<60&&(int)(Math.random()*3)==2){
+			Begin.EMV -= 10+(int)(Math.random()*4)*2;
 			Begin.HP -= 5*(int)(Math.random()*4);
 			if(Begin.HP<=0)
 				Begin.HP = 0;
-			s += enviroment[(int)(Math.random()*4)] + "\n";
+			s += enviroment2[(int)(Math.random()*4)] + "\n";
 		}
 		
 		return s;
