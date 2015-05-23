c6bd0852560b6f5c4da95f70cfcdb1560e403f7f
bismuth1102 <951102ljc>
2015-05-23 13:24:14 +0800

    OVER!!

--- Game1/src/el/Begin.java
+++ Game1/src/el/Begin.java
@@ -1,10 +1,10 @@
 package el;
 
 public class Begin {
-	public static int EV = 10000;
+	public static int EV = 100;
 	public static int EMV = 80;
 	public static int HV = 80;
 	public static int SV = 0;
-	public static int HP = 20;
+	public static int HP = 15;
 
 }
